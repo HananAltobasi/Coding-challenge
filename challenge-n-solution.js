6c7444bfd7efe57a020b6443d7eb816170786a40
hanan omar altobasi <hananaltobase96@gmail.com>
2021-03-30 13:36:57 +0300

    solve

---
+++ challenge-n-solution.js
@@ -0,0 +1,29 @@
+// Create a function that gets numbers from an array and logs the square root of each number.
+'use strict'
+let array=[2,4,6,8,10];
+function getNumber(){
+    document.getElementById("num").innerHTML=Math.SQRT2(array);
+}
+getNumber();
+
+
+
+//Create a program that takes 5 numeric values from user input and alerts back the minimum and maximum of the 5 provided numbers.
+function Number(Number1,Number2,Number3,Number4,Number5){
+this.Number1=Number1;
+this.Number2=Number2;
+this.Number3=Number3;
+this.Number4=Number4;
+this.Number5=Number5;
+Number.all.push(this);
+}
+Number.all=[];
+
+for(let i=0;i<Number.all.length;i++){
+if(Number1<Number2 || Number1<Number3 || Number1<Number4 || Number1<Number5){
+    Number1.innerHTML;
+}
+}
+
+
+
